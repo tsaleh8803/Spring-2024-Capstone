40bb4df38e7dd6ef67a8bed76d1e729e553701b9
edwardpresdec <118995757+edwardpresdec@users.noreply.github.com>
2024-02-29 19:38:21 +0200

    knee angle detection and collection

--- testproject.py
+++ testproject.py
@@ -1,13 +1,12 @@
 import mediapipe as mp
 import cv2
 import numpy as np
+import csv
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 mp_pose = mp.solutions.pose
 cap = cv2.VideoCapture(0)
-
-#HI
 
 def calculate_angle(a,b,c):
     a = np.array(a) # First
@@ -22,6 +21,13 @@ def calculate_angle(a,b,c):
         
     return angle 
 
+buffer = [0 for element in range(60)]
+buffer1 = []
+buffer1.extend(buffer)
+tocsv = [1]
+overall_count = 0
+flag = False
+
 #initiate holistic model
 with mp_holistic.Holistic(min_detection_confidence = 0.5,min_tracking_confidence = 0.5) as holistic:
     while cap.isOpened():
@@ -33,20 +39,47 @@ with mp_holistic.Holistic(min_detection_confidence = 0.5,min_tracking_confidence
         results = holistic.process(image)
         image_height, image_width, _ = image.shape
         #convert image back to BGR to process it
-        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)        
+        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
 
         try:
             landmarks = results.pose_landmarks.landmark
+            hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
             shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
             elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
             wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+            ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
             
-            # Calculate angle
+            # Calculate angles
             angle = int(calculate_angle(shoulder, elbow, wrist))
+            angle1 = int(calculate_angle(hip, knee, ankle))
             
-            # Visualize angle
-            cv2.putText(image, "Elbow angle: " + str(angle),(100,100), 
+            # Visualize required angle
+
+            cv2.putText(image, "Knee angle: " + str(angle1),(100,100), 
                            cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 3, cv2.LINE_AA)
+            
+            #When the knee angle dips under 70 degrees, the buffer starts recording values
+
+            if(angle1<120 or flag):
+                if(overall_count==len(buffer)):
+                    print("STOP")
+                    minimum = min(buffer)
+                    pos = buffer.index(minimum)
+                    tocsv[0] = angle
+                    with open('angles.csv', 'a', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(tocsv)
+                    overall_count=0
+                    flag = False
+                else:
+                    print("HI")
+                    flag = True
+                    buffer[overall_count] = hip[1]
+                    buffer[overall_count] = angle
+                    overall_count+=1
+
+
         except:
             pass
 
